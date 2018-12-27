174e214d534773385dabd18a90a774c4e160bd5c
Caio Farias <caiohenriquefarias16@gmail.com>
2018-12-27 13:00:33 -0300

    adicionar telefone

--- classes/UsuarioDAO.class.php
+++ classes/UsuarioDAO.class.php
@@ -1,7 +1,7 @@
 
 <?php
 
-require_once('../Database.class.php');
+require_once('Database.class.php');
 
 
 class UsuarioDAO{
--- php/insert_telefone.php
+++ php/insert_telefone.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('../dominio/UsuarioDAO.php');
+    require_once('../classes/UsuarioDAO.php');
     $numero = $_POST['numero'];
     echo($numero);
 
